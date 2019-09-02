6561d43269a486ea98b3dd6de271ece5169ac699
ozandabb <osanda.gamage.79@gmail.com>
2019-09-02 18:51:02 +0530

    view button

--- app/src/main/java/com/example/student/studentmain/DatabaseHelper.java
+++ app/src/main/java/com/example/student/studentmain/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.example.student.studentmain;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -42,5 +43,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             return false;
         else
             return true;
+    }
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from "+TABLE_NAME,null);
+        return res;
     }
 }
--- app/src/main/java/com/example/student/studentmain/MainActivity.java
+++ app/src/main/java/com/example/student/studentmain/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.student.studentmain;
 
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -26,7 +28,9 @@ public class MainActivity extends AppCompatActivity {
         editSurname = (EditText)findViewById(R.id.editText_surename);
         editMarks = (EditText)findViewById(R.id.editText_marks);
         btnAddData = (Button)findViewById(R.id.add);
+        btnviewAll = (Button)findViewById(R.id.button_viewAll);
         AddData();
+        viewAll();
     }
 
     public  void AddData() {
@@ -45,4 +49,41 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
     }
+
+
+    public void viewAll() {
+        btnviewAll.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Cursor res = mydb.getAllData();
+                        if(res.getCount() == 0) {
+                            // show message
+                            showMessage("Error","Nothing found");
+                            return;
+                        }
+
+                        StringBuffer buffer = new StringBuffer();
+                        while (res.moveToNext()) {
+                            buffer.append("Id :"+ res.getString(0)+"\n");
+                            buffer.append("Name :"+ res.getString(1)+"\n");
+                            buffer.append("Surname :"+ res.getString(2)+"\n");
+                            buffer.append("Marks :"+ res.getString(3)+"\n\n");
+                        }
+
+                        // Show all data
+                        showMessage("Data",buffer.toString());
+                    }
+                }
+        );
+    }
+
+    public void showMessage(String title,String Message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+    }
+
 }
